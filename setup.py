83e130339b84d24b454f24e0f4198ab737046b75
Nikolaos Apostolakos <nikoapos@gmail.com>
2017-11-03 09:50:52 +0100

    Add the setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup (
+       name='NNPZ',
+       version='0.1',
+       packages=find_packages(),
+
+       # Declare your packages' dependencies here, for eg:
+       install_requires=[],
+
+       # Fill in these to make your Egg ready for upload to
+       # PyPI
+       author='Nikolaos Apostolakos <nikoapos@gmail.com>',
+       author_email='',
+
+       #summary = 'Just another Python package for the cheese shop',
+       url='https://github.com/nikoapos/NNPZ',
+       license='MIT',
+       long_description='Nearest Neighbors Photometric Redshift',
+
+       # could also include long_description, download_url, classifiers, etc.
+
+  
+       )
+
