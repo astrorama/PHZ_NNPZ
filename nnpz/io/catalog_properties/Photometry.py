b69e853d686f7c94bb8c08c3b66ae34ffb67679a
Nikolaos Apostolakos <nikoapos@gmail.com>
2018-03-12 18:27:55 +0100

    Add the option to set the flags for the missing photometries in the target catalog

--- nnpz/io/catalog_properties/Photometry.py
+++ nnpz/io/catalog_properties/Photometry.py
@@ -15,18 +15,21 @@ class Photometry(CatalogReader.CatalogPropertyInterface):
     """Catalog property for retrieving the photometry at NNPZ format"""
 
 
-    def __init__(self, column_list):
+    def __init__(self, column_list, nan_flags=[]):
         """Creates a new instance for the given column names.
 
         Args:
             column_list: A list of tuples with the columns containing the
                 photometry. The first element of the tuple is the name of the
                 value column and the second the error column.
+            nan_flags: A list of values which when found in the photometry
+                columns are replaced with NaN
 
         Note: The error columns are allowed to be None, in which case the errors
         of the band will be set to 0.
         """
         self.__column_list = column_list
+        self.__nan_flags = nan_flags
 
 
     def __call__(self, catalog):
@@ -61,5 +64,9 @@ class Photometry(CatalogReader.CatalogPropertyInterface):
             data[:,i,0] = catalog[value]
             if not error is None:
                 data[:,i,1] = catalog[error]
+
+        # Replace all the NaN flags
+        for flag in self.__nan_flags:
+            data[data==flag] = np.nan
 
         return data
