a72d7b278b199004b654845a0c43f77c604aa6ae
Nikolaos Apostolakos <nikoapos@gmail.com>
2018-03-15 18:29:35 +0100

    Add the median true redshift as an output

---
+++ nnpz/io/output_column_providers/MedianTrueRedshift.py
@@ -0,0 +1,38 @@
+"""
+Created on: 15/02/18
+Author: Nikolaos Apostolakos
+"""
+
+from __future__ import division, print_function
+
+import numpy as np
+from astropy.table import Column
+
+from nnpz.io import OutputHandler
+
+
+class MedianTrueRedshift(OutputHandler.OutputColumnProviderInterface):
+
+    def __init__(self, catalog_size, ref_true_redshift_list):
+        self.__ref_z = ref_true_redshift_list
+        self.__zs = [[] for i in range(catalog_size)]
+        self.__weights = [[] for i in range(catalog_size)]
+
+
+    def addContribution(self, reference_sample_i, catalog_i, weight):
+        z = self.__ref_z[reference_sample_i]
+        self.__zs[catalog_i].append(z)
+        self.__weights[catalog_i].append(weight)
+
+
+    def getColumns(self):
+        median_z = np.zeros(len(self.__zs), dtype=np.float32)
+        for i, (z, w) in enumerate(zip(self.__zs, self.__weights)):
+            half = sum(w) / 2.
+            c = 0
+            for sort_i in np.argsort(w):
+                c += w[sort_i]
+                if c > half:
+                    median_z[i] = z[sort_i]
+        col = Column(median_z, 'MedianTrueZ')
+        return [col]
--- nnpz/io/output_column_providers/__init__.py
+++ nnpz/io/output_column_providers/__init__.py
@@ -3,3 +3,4 @@ from .NeighborList import NeighborList
 from .TrueRedshiftPdz import TrueRedshiftPdz
 from .MeanPhotometry import MeanPhotometry
 from .FullCatalogCopy import FullCatalogCopy
+from .MedianTrueRedshift import MedianTrueRedshift
