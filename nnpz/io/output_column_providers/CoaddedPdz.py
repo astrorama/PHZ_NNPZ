2cbe471ebefe9d8ab423678c72e70e7aec1fafb2
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2018-03-28 16:37:26 +0200

    Task #15518: PDZ as weighted sum of neighbors PDZ

---
+++ nnpz/io/output_column_providers/CoaddedPdz.py
@@ -0,0 +1,42 @@
+"""
+Created on: 28/03/2018
+Author: Alejandro Alvarez Ayllon
+"""
+
+from __future__ import division, print_function
+
+import numpy as np
+from astropy.table import Column
+from nnpz.io import OutputHandler
+
+
+class CoaddedPdz(OutputHandler.OutputColumnProviderInterface):
+
+    def __init__(self, catalog_size, reference_sample, ref_ids):
+        super(CoaddedPdz, self).__init__()
+        self.__reference_sample = reference_sample
+        self.__pdz_bins = reference_sample.getPdzData(ref_ids[0])[:, 0]
+        self.__pdzs = np.zeros((catalog_size, len(self.__pdz_bins)), dtype=np.float32)
+        self.__ref_ids = ref_ids
+        self.__current_ref_i = None
+        self.__current_ref_pdz = None
+
+    def addContribution(self, reference_sample_i, catalog_i, weight):
+        if reference_sample_i != self.__current_ref_i:
+            ref_id = self.__ref_ids[reference_sample_i]
+            self.__current_ref_i = reference_sample_i
+            self.__current_ref_pdz = self.__reference_sample.getPdzData(ref_id)
+            assert (self.__current_ref_pdz[:, 0] == self.__pdz_bins).all()
+
+        self.__pdzs[catalog_i] += self.__current_ref_pdz[:, 1] * weight
+
+    def getColumns(self):
+        integrals = 1. / np.trapz(self.__pdzs, self.__pdz_bins, axis=1)
+        normalized = (self.__pdzs.T * integrals).T
+        return [
+            Column(normalized, 'CoaddedPdz'),
+            Column([self.__pdz_bins] * normalized.shape[0], 'CoaddedPdzBins'),
+        ]
+
+    def getPdzBins(self):
+        return self.__pdz_bins
