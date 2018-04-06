f7c1466e191e48d80a6d76fbec1f9da174a16284
Nikolaos Apostolakos <nikoapos@gmail.com>
2018-04-06 18:42:44 +0200

    Add to the ouput the PDZ quantiles and MC sampling

--- nnpz/config/PdzOutputConfig.py
+++ nnpz/config/PdzOutputConfig.py
@@ -13,7 +13,7 @@ import nnpz.io.output_column_providers as ocp
 class PdzOutputConfig(ConfigManager.ConfigHandler):
 
     def __init__(self):
-        self.__pdz_column_prov = None
+        self.__added = False
 
     def __addColumnProvider(self, args):
         target_ids = ConfigManager.getHandler(TargetCatalogConfig).parseArgs(args)['target_ids']
@@ -24,22 +24,29 @@ class PdzOutputConfig(ConfigManager.ConfigHandler):
         # this case the PDZ is the weighted co-add of the sample PDZs.
         if 'reference_sample' in ref_options:
             ref_sample = ref_options['reference_sample']
-            self.__pdz_column_prov = ocp.CoaddedPdz(len(target_ids), ref_sample, ref_ids)
+            pdz_col = 'CoaddedPdz'
+            pdz_prov = ocp.CoaddedPdz(len(target_ids), ref_sample, ref_ids)
 
         # Now we handle the case where we have a reference catalog. In this case
         # the PDZ is the normalized histogram of the neighbors redshifts.
         if 'reference_redshift' in ref_options:
             ref_z = ref_options['reference_redshift']
-            self.__pdz_column_prov = ocp.TrueRedshiftPdz(len(target_ids), ref_z, 0, 6, 601)
+            pdz_col = 'TrueRedshiftPDZ'
+            pdz_prov = ocp.TrueRedshiftPdz(len(target_ids), ref_z, 0, 6, 601)
 
         output = ConfigManager.getHandler(OutputHandlerConfig).parseArgs(args)['output_handler']
-        output.addColumnProvider(self.__pdz_column_prov)
+        output.addColumnProvider(pdz_prov)
+
+        pdz_quantiles = args.get('pdz_quantiles', [])
+        pdz_mc_samples = args.get('pdz_mc_samples', 0)
+        output.addColumnProvider(ocp.PdfSampling(pdz_prov, pdz_col, quantiles=pdz_quantiles, mc_samples=pdz_mc_samples))
 
 
     def parseArgs(self, args):
-        if self.__pdz_column_prov is None:
+        if not self.__added:
             self.__addColumnProvider(args)
-        return {'pdz_column_provider' : self.__pdz_column_prov}
+            self.__added = True
+        return {}
 
 
 ConfigManager.addHandler(PdzOutputConfig)
