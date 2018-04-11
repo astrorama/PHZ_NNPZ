bc087bf2e73d1164b783d10740a9b98e49b67800
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2018-04-11 09:15:40 +0200

    Trivial test_copiedPhotometry

---
+++ tests/weights/test_CopiedPhotometry.py
@@ -0,0 +1,26 @@
+"""
+Created: 11/04/2018
+Author: Alejandro Alvarez Ayllon
+"""
+
+from __future__ import division, print_function
+
+from nnpz.photometry import PhotometryProvider
+from nnpz.weights import CopiedPhotometry
+from tests.photometry.fixtures import *
+
+
+###############################################################################
+
+def test_copiedPhotometryAll(photometry_file_fixture):
+
+    # Given
+    provider = PhotometryProvider(photometry_file_fixture)
+
+    # When
+    copied = CopiedPhotometry(provider.getData())
+
+    # Then
+    for ref_i in range(len(provider.getIds())):
+        phot = copied(ref_i, 1)
+        assert np.array_equal(phot, provider.getData()[ref_i])
