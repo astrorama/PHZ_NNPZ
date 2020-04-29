9c4d3d75de411385688cd15958965bbb1cb2adf3
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-04-29 11:48:30 +0200

    Add directed distance for comparison

--- NNPZ/python/nnpz/neighbor_selection/brute_force_methods/__init__.py
+++ NNPZ/python/nnpz/neighbor_selection/brute_force_methods/__init__.py
@@ -1,5 +1,6 @@
 from .Chi2Distance import Chi2Distance
 from .EuclideanDistance import EuclideanDistance
 from .ScaledChi2Distance import ScaledChi2Distance
+from .DirectedDistance import DirectedDistance
 from .SmallestSelector import SmallestSelector
 from .LessThanSelector import LessThanSelector
