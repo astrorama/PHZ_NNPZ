1aaaa69c7f1b99bcf8e003b7b4334106f146b07f
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-04-29 15:50:27 +0200

    Move outside the loop part of the computation

--- NNPZ/python/nnpz/neighbor_selection/brute_force_methods/ScaledChi2Distance.py
+++ NNPZ/python/nnpz/neighbor_selection/brute_force_methods/ScaledChi2Distance.py
@@ -22,21 +22,23 @@ class ScaledChi2Distance(BruteForceSelector.DistanceMethodInterface):
         self.__a_min = a_min
         self.__a_max = a_max
 
-    def __call__(self, ref_data_values, ref_data_errors, coord_values, coord_errors):
+    def __call__(self, ref_values, ref_errors, coord_values, coord_errors):
         """
         Minimizes the chi2 distance using the scale factor a, which is constrained by
         the prior passed to the constructor
         """
 
-        d = np.ones(len(ref_data_values))
-        a = np.ones(len(ref_data_values))
+        # Do an informed guess
+        a = np.sum(ref_values * coord_values / coord_errors ** 2, axis=1) / np.sum(ref_values ** 2 / coord_errors ** 2,
+                                                                                   axis=1)
 
+        # Treat each one individually
         for i in range(len(a)):
             # Do an educated guess ignoring the errors
-            ri = ref_data_values[i, :]
+            ri = ref_values[i, :]
+            sri = ref_errors[i, :]
             fi = coord_values
             si = coord_errors
-            sri = ref_data_errors[i, :]
 
             def chi2(a):
                 nom = (a * ri[:, np.newaxis] - fi[:, np.newaxis]) ** 2
@@ -46,19 +48,18 @@ class ScaledChi2Distance(BruteForceSelector.DistanceMethodInterface):
             def likelihood(a):
                 return np.exp(-chi2(a) / 2)
 
-            # Do an informed guess
-            a_guess = np.sum(ri * fi / si ** 2) / np.sum(ri ** 2 / si ** 2)
-
             # If the guess is out of bounds, clip and finish
-            if a_guess <= self.__a_min:
+            if a[i] <= self.__a_min:
                 a[i] = self.__a_min
-            elif a_guess >= self.__a_max:
+            elif a[i] >= self.__a_max:
                 a[i] = self.__a_max
             # If it is within, do the maximization of the likelihood * prior
             else:
                 # Which is done minimizing the negative
-                a[i] = fmin(lambda a: -self.__prior(a) * likelihood(a), a[i], disp=False)[0]
-            # Compute the chi2 distance for the given a
-            d[i] = chi2(a[i])
+                a[i] = fmin(lambda a: -self.__prior(a) * likelihood(a), a[i], disp=False, maxiter=10, ftol=1e-2)[0]
 
+        # Compute the final distances
+        nom = (a[:, np.newaxis] * ref_values - coord_values) ** 2
+        den = (a[:, np.newaxis] * ref_errors) ** 2 + coord_errors ** 2
+        d = np.sum(nom / den, axis=1)
         return d, a
