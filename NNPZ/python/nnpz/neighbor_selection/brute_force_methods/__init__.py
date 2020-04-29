48ea62612b3d65c2a19073082e99cc5b670ebd48
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-04-29 12:56:37 +0200

    Add AngularDistance for comparison

--- NNPZ/python/nnpz/neighbor_selection/brute_force_methods/__init__.py
+++ NNPZ/python/nnpz/neighbor_selection/brute_force_methods/__init__.py
@@ -2,5 +2,6 @@ from .Chi2Distance import Chi2Distance
 from .EuclideanDistance import EuclideanDistance
 from .ScaledChi2Distance import ScaledChi2Distance
 from .DirectedDistance import DirectedDistance
+from .AngularDistance import AngularDistance
 from .SmallestSelector import SmallestSelector
 from .LessThanSelector import LessThanSelector
