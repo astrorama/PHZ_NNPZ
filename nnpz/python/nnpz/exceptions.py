525dcf2dc1157f939ae92ef0746f84d760400483
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-07-15 15:13:45 +0200

    Remove unneeded pass, remove unused exception

--- nnpz/python/nnpz/exceptions.py
+++ nnpz/python/nnpz/exceptions.py
@@ -25,109 +25,87 @@ class NnpzException(Exception):
     """
     Base class for NNPZ specific exceptions
     """
-    pass
 
 
 class DuplicateIdException(NnpzException):
     """
     The reference sample already has an object with the given ID
     """
-    pass
 
 
 class IdMismatchException(NnpzException):
     """
     Tried to set an attribute for an object ID that has not been registered
     """
-    pass
 
 
 class InvalidDimensionsException(NnpzException):
     """
     The shape and/or dimensionality of a numpy array does not match the expectations
     """
-    pass
 
 
 class InvalidAxisException(NnpzException):
     """
     The axis values of an array do not match the expectations
     """
-    pass
 
 
 class AlreadySetException(NnpzException):
     """
     Tried to re-define an attribute on an existing reference object
     """
-    pass
 
 
 class UninitializedException(NnpzException):
     """
     The initialization sequence has not been done or done incorrectly
     """
-    pass
 
 
 class FileNotFoundException(NnpzException):
     """
     File does not exist
     """
-    pass
 
 
 class UnknownNameException(NnpzException):
     """
     An input table misses some required attributes
     """
-    pass
-
-
-class AmbiguityException(NnpzException):
-    """
-    TODO: Unused
-    """
-    pass
 
 
 class CorruptedFileException(NnpzException):
     """
     Either the file has been corrupted, or received a file with the wrong format
     """
-    pass
 
 
 class InvalidPositionException(CorruptedFileException):
     """
     The file format is not corrupted, but an index value is invalid
     """
-    pass
 
 
 class InvalidPathException(NnpzException):
     """
     Equivalent to NotADirectoryError
     """
-    pass
 
 
 class WrongTypeException(NnpzException):
     """
     A class does not implement an expected method
     """
-    pass
 
 
 class WrongFormatException(NnpzException):
     """
     A file does not follow the expected format
     """
-    pass
 
 
 class MissingDataException(NnpzException):
     """
     Required data is not present
     """
-    pass
