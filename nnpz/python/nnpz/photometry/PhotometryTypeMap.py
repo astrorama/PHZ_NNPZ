f048eec201be7ca7a95aa40ca2d4da96a684b4b8
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2021-11-09 14:06:20 +0100

    Drop flambda, magab and photon count
    Latests developments are not adapted to them

--- nnpz/python/nnpz/photometry/PhotometryTypeMap.py
+++ nnpz/python/nnpz/photometry/PhotometryTypeMap.py
@@ -21,13 +21,9 @@ Author: Nikolaos Apostolakos
 
 from __future__ import division, print_function
 
-from nnpz.photometry import PhotonPrePostProcessor, FnuPrePostProcessor, FnuuJyPrePostProcessor, \
-    MagAbPrePostProcessor, FlambdaPrePostProcessor
+from nnpz.photometry import FnuPrePostProcessor, FnuuJyPrePostProcessor
 
 PhotometryTypeMap = {
-    'Photons': (PhotonPrePostProcessor, 'Photon count rate in counts/s/cm^2'),
     'F_nu': (FnuPrePostProcessor, 'Energy flux density in erg/s/cm^2/Hz'),
     'F_nu_uJy': (FnuuJyPrePostProcessor, 'Energy flux density in uJy'),
-    'MAG_AB': (MagAbPrePostProcessor, 'AB magnitude'),
-    'F_lambda': (FlambdaPrePostProcessor, 'Energy flux density in erg/s/cm^2/A')
 }
