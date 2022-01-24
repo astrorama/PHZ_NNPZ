8dcee8cb0418fb8e6847f2c32c55066993cd196c
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-01-24 15:13:30 +0100

    Correct all bands

--- nnpz/python/nnpz/pipeline/correct_photometry.py
+++ nnpz/python/nnpz/pipeline/correct_photometry.py
@@ -49,6 +49,7 @@ logger = Logging.getLogger(__name__)
 
 class CorrectPhotometry:
     def __init__(self, conf_manager: ConfigManager):
+        self.__ref_system = conf_manager.getObject('reference_system')
         self.__ebv_corr_coefs = conf_manager.getObject('reference_ebv_correction')
         self.__filter_corr_coefs = conf_manager.getObject('reference_filter_variation_correction')
 
@@ -58,11 +59,15 @@ class CorrectPhotometry:
         if out is None:
             out = neighbor_photo
 
+        assert out.shape == neighbor_photo.shape
+        assert out.shape[0] == len(target)
+        assert out.shape[2] == len(self.__ref_system.bands)
+        assert out.shape[3] == 2
+
         if 'ebv' in target.colorspace:
-            logger.info('Correcting for EBV')
             chunk_ebv_corr_coefs = self.__ebv_corr_coefs[neighbor_idx]
-            # TODO: Correct *all* reference bands
-            for filter_idx, filter_name in enumerate(target.system.bands):
+            for filter_idx, filter_name in enumerate(self.__ref_system.bands):
+                logger.info('Correcting %s for EBV', filter_name)
                 nn_filter_photo = neighbor_photo[:, :, filter_idx, :]
                 nn_filter_out = out[:, :, filter_idx, :]
                 correct_ebv(nn_filter_photo,
@@ -72,7 +77,7 @@ class CorrectPhotometry:
 
         if 'shifts' in target.colorspace:
             chunk_filter_corr_coefs = self.__filter_corr_coefs[neighbor_idx]
-            for filter_idx, filter_name in enumerate(target.system.bands):
+            for filter_idx, filter_name in enumerate(self.__ref_system.bands):
                 if filter_name not in target.colorspace.shifts.dtype.names:
                     continue
                 logger.info('Correcting for %s filter variation', filter_name)
@@ -81,5 +86,3 @@ class CorrectPhotometry:
                                          corr_coef=chunk_filter_corr_coefs[:, :, filter_idx],
                                          shift=target.colorspace.shifts[filter_name],
                                          out=nn_filter_out)
-
-        # TODO: Rescale
