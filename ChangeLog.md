0de160c6675da7ceabb46c3527a72212198c260b
Florian Dubath <florian.dubath@unige.ch>
2023-07-19 10:57:05 +0200

    Fix Scaling bug

---
+++ ChangeLog.md
@@ -0,0 +1,13 @@
+# Changelog
+
+Project Coordinators: Florian Dubath @fdubath
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+
+## [1.2.2] 2023-07-19
+### Fixed
+- Fix the scaling case: the scale was applied twice causing the weight to be off 
+
+
--- nnpz/python/nnpz/pipeline/compute_weights.py
+++ nnpz/python/nnpz/pipeline/compute_weights.py
@@ -51,7 +51,8 @@ class ComputeWeights:
                  neighbor_scales: np.ndarray, out_weights: np.ndarray, out_flags: np.ndarray):
         # This makes a copy
         nn_photo = neighbor_photo.take(self.__ref_filter_indexes, axis=2)
-        nn_photo *= neighbor_scales[..., np.newaxis, np.newaxis]
+        # The Scaling of the photometry is done by the neighbor finder
+        # nn_photo *= neighbor_scales[..., np.newaxis, np.newaxis]
 
         # FITS stores in big-endian, and we can't pass that directly to cython
         if nn_photo.dtype.byteorder == '>':
